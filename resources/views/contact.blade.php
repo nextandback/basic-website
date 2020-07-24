d9a7f59c20ecdf0a4087eb64368b50dd6c892d18
Mehmet Dogukan Sevinc <nextandbackacademy@gmail.com>
2020-07-24 20:11:18 +0300

    Third

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,41 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Contact</h1>
+    <h1>Contact Us</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+
+    <form method="post" action="{{route('contact-form-submit')}}">
+        @csrf
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name"  placeholder="Enter name">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="text" class="form-control" id="email" name="email" placeholder="Enter email">
+        </div>
+
+        <div class="form-group">
+            <label for="subject">Subject</label>
+            <input type="text" class="form-control" id="subject" name="subject"  placeholder="Enter subject">
+        </div>
+
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea class="form-control" name="message" id="message" rows="3" placeholder="Enter your message"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
